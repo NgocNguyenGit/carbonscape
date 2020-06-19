8bc026cff8f2c00f34f73b031377d2e2bb0a362f
Deepjyoti Paul <deepjyotipaul69@gmail.com>
2020-06-19 15:14:06 +0530

    maps added

--- pages/index.js
+++ pages/index.js
@@ -1,209 +1,76 @@
-import Head from 'next/head'
+import React, { Component } from "react";
+// import * as atlas from "azure-maps-control";
+import Head from "next/head";
 
-export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+export class index extends Component {
+	componentDidMount() {
+		var map, datasource;
 
-      <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+		map = new atlas.Map("myMap", {
+			center: [35.9078, 127.7669],
+			zoom: 3,
+      language: "en-US",
+      style: 'grayscale_dark',
+			authOptions: {
+				authType: "subscriptionKey",
+				subscriptionKey: "C570VXYfVEQCjJ-_EyYYIMIlM_PJOb6QUxIls-0Dmmw",
+			},
+		});
+		map.events.add("ready", function () {
+			//Create a data source and add it to the map.
+			datasource = new atlas.source.DataSource(null, {
+				//Tell the data source to cluster point data.
+				cluster: true,
 
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+				//The radius in pixels to cluster points together.
+				clusterRadius: 15,
+			});
+			map.sources.add(datasource);
 
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+			//Create a heatmap and add it to the map.
+			map.layers.add(
+				new atlas.layer.HeatMapLayer(datasource, null, {
+					//Set the weight to the point_count property of the data points.
+					weight: ["get", "point_count"],
 
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+					//Optionally adjust the radius of each heat point.
+					radius: 20,
+				}),
+				"labels"
+			);
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
-  )
+			//Load a data set of points, in this case earthquake data from the USGS.
+			datasource.importDataFromUrl(
+				"https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson"
+			);
+		});
+	}
+	render() {
+		return (
+			<>
+				<Head>
+					<link
+						rel="stylesheet"
+						href="https://atlas.microsoft.com/sdk/javascript/mapcontrol/2/atlas.min.css"
+						type="text/css"
+					/>
+					<script src="https://atlas.microsoft.com/sdk/javascript/mapcontrol/2/atlas.min.js"></script>
+					<style jsx>
+						{`
+							body {
+								margin: 0;
+								padding: 0;
+							}
+						`}
+					</style>
+				</Head>
+				<div
+					id="myMap"
+					style={{ height: "100vh", width: "100%" }}
+				></div>
+			</>
+		);
+	}
 }
+
+export default index;
